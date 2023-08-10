ac10d411153ad816f7ec640b27371472eadcc488
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 20:16:28 +0200

    Ejemplo tabla multiplicar del 5 al pulsar el boton mostramos

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,8 @@
 <template>
   <div>
-    <!-- Sección para agregar usuarios -->
-    <h2>Agregar Usuarios</h2>
-    <form @submit.prevent="agregarUsuario">
-      <input v-model="nuevoUsuario" placeholder="Nombre de usuario">
-      <button>Añadir Usuario</button>
-    </form>
-
-    <!-- Sección para mostrar/ocultar la lista de usuarios -->
-    <h2>Lista de Usuarios</h2>
-    <button @click="toggleLista">{{ mostrarLista ? 'Ocultar' : 'Mostrar' }} Lista</button>
-
-    <!-- Lista de usuarios con botones para borrar y guardar -->
-    <ul v-if="mostrarLista">
-      <li v-for="(usuario, index) in usuarios" :key="index">
-        {{ usuario }}
-        <button @click="borrarUsuario(index)">Borrar</button>
-        <button @click="guardarUsuario(index)">Guardar</button>
-      </li>
-    </ul>
-
-    <!-- Sección para mostrar/ocultar la lista de usuarios guardados -->
-    <h2>Lista Guardada</h2>
-    <button @click="toggleListaGuardada">{{ mostrarListaGuardada ? 'Ocultar' : 'Mostrar' }} Lista Guardada</button>
-
-    <!-- Lista de usuarios guardados -->
-    <ul v-if="mostrarListaGuardada">
-      <li v-for="(usuario, index) in usuariosGuardados" :key="index">{{ usuario }}</li>
-    </ul>
+    <h2>Tabla de Multiplicar del Nº 5</h2>
+    <textarea v-model="tablaResultado" rows="10" cols="50" readonly></textarea>
+    <button @click="generarTabla">Generar Tabla del 5</button>
   </div>
 </template>
 
@@ -35,38 +10,28 @@
 export default {
   data() {
     return {
-      nuevoUsuario: '',
-      usuarios: ['Usuario 1', 'Usuario 2', 'Usuario 3'],
-      usuariosGuardados: [],
-      mostrarLista: false,
-      mostrarListaGuardada: false
+      tablaResultado: ''
     };
   },
   methods: {
-    // Agregar un nuevo usuario a la lista
-    agregarUsuario() {
-      if (this.nuevoUsuario.trim() !== '') {
-        this.usuarios.push(this.nuevoUsuario);
-        this.nuevoUsuario = ''; // Limpiamos el campo de entrada
+    generarTabla() {
+      const numero = 5; // Puedes cambiar este número según tu necesidad
+      let resultado = `# Tabla del ${numero} #\n`;
+
+      for (let i = 0; i <= 10; i++) {
+        let multiplicacion = i * numero;
+        resultado += `${i} x ${numero} = ${multiplicacion}\n`;
       }
-    },
-    // Borrar un usuario de la lista
-    borrarUsuario(index) {
-      this.usuarios.splice(index, 1);
-    },
-    // Guardar un usuario en la lista de usuarios guardados
-    guardarUsuario(index) {
-      const usuario = this.usuarios[index];
-      this.usuariosGuardados.push(usuario);
-    },
-    // Alternar la visibilidad de la lista de usuarios
-    toggleLista() {
-      this.mostrarLista = !this.mostrarLista;
-    },
-    // Alternar la visibilidad de la lista de usuarios guardados
-    toggleListaGuardada() {
-      this.mostrarListaGuardada = !this.mostrarListaGuardada;
+
+      this.tablaResultado = resultado;
     }
   }
 };
 </script>
+
+<style>
+/* Estilos opcionales para textarea */
+textarea {
+  margin-top: 10px;
+}
+</style>
