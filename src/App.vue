ac5ef8979c00e0e1974f26abab51ddcb3b2dc580
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 20:26:32 +0200

    Ejemplo polindromo completo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <h2>Verificar Palíndromo</h2>
-    <input v-model="inputText" placeholder="Ingrese una cadena de texto">
+    <input ref="inputField" v-model="inputText" :disabled="verificando" placeholder="Ingrese una cadena de texto">
     <button @click="verificarPalindromo">Verificar</button>
-    <p>{{ resultado }}</p>
+    <p v-if="mostrarResultado">{{ resultado }}</p>
+    <p v-if="mostrarMensaje">{{ mensaje }}</p>
   </div>
 </template>
 
@@ -12,14 +13,38 @@ export default {
   data() {
     return {
       inputText: '',
-      resultado: ''
+      resultado: '',
+      verificando: false,
+      mostrarResultado: false,
+      mostrarMensaje: false,
+      mensaje: ''
     };
   },
   methods: {
     verificarPalindromo() {
-      const texto = this.inputText.toLowerCase().replace(/\s/g, ''); // Convertir a minúsculas y quitar espacios
+      if (this.inputText.trim() === '') {
+        this.mostrarMensaje = true;
+        this.mensaje = 'Por favor, ingrese un texto';
+        return;
+      }
+
+      this.mostrarMensaje = false;
+      this.verificando = true;
+      const texto = this.inputText.toLowerCase().replace(/\s/g, '');
       const invertido = texto.split('').reverse().join('');
       this.resultado = invertido === texto ? 'Es un palíndromo' : 'No es un palíndromo';
+      this.mostrarResultado = true;
+
+      setTimeout(() => {
+        this.verificando = false;
+        this.inputText = '';
+        this.resultado = '';
+        this.mostrarResultado = false;
+
+        this.$nextTick(() => {
+          this.$refs.inputField.focus();
+        });
+      }, 3000);
     }
   }
 };
@@ -31,3 +56,8 @@ input {
   margin-right: 10px;
 }
 </style>
+
+
+<!-- En este ejemplo, hemos agregado una nueva propiedad mostrarMensaje para controlar si se debe mostrar el mensaje de advertencia. Dentro del método verificarPalindromo, verificamos si el inputText está en blanco usando trim(), y si lo está, mostramos el mensaje y detenemos la ejecución del método con return. Si se ingresó algún texto, ocultamos el mensaje. De esta manera, se mostrará el mensaje de advertencia si el usuario intenta verificar sin ingresar nada. -->
+
+<!-- Utilizamos $nextTick() en conjunto con focus() dentro del setTimeout(). Esto asegura que el enfoque se aplique después de que Vue haya actualizado la vista y que el cursor permanezca dentro del input después de 3 segundos. -->
